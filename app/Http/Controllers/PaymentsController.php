254d1fa15b496e83d90da6ba751c97a972799518
marcellus-jeanbernard <tutojeanbernard@gmail.com>
2018-11-03 18:39:21 -0400

    first commit

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Session;
 use Illuminate\Http\Request;
 use Auth;
 class PaymentsController extends Controller
@@ -9,8 +9,16 @@ class PaymentsController extends Controller
     
     public function pay(Request $request, $plan){
     	
-    	 Auth::user()->newSubscription('primary', $plan)->create($request->stripeToken);
+    	  // Auth::user()->newSubscription('primary', $plan)->create($request->stripeToken);
+    	//$user = Auth::user();
+               try {
+                Auth::user()->newSubscription('primary', $plan)->create($request->stripeToken);
+               	
     	
+               } catch (\Exception $e) {
+               	Session::flash('error_message', 'There has been an error processing your payment.');
+        // return redirect('/');
+               }
     	return 'Subscribed!';
     }
 }
